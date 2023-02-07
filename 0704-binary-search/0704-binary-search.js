a29a85f3b5373fb201bc83ff3ca2b204309b0802
Shea <sheae123@gmail.com>
2023-02-07 10:26:52 -0800

    Time: 70 ms (67.49%), Space: 44.1 MB (99.06%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,16 @@
+function search (nums, target) {
+    start = 0
+    end = nums.length - 1
+    
+    while (start <= end) {
+        mid = Math.floor((end + start) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            start = mid + 1
+        } else {
+            end = mid - 1
+        }
+    }
+    return - 1
+}
