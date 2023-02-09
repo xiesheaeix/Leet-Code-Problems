5681c791e36862191a4ccb58555721e266382f15
Shea <sheae123@gmail.com>
2023-02-09 14:09:30 -0800

    Time: 86 ms (92.07%), Space: 46.2 MB (87.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -1,9 +1,9 @@
 function moveZeroes(nums) {
-    let j = 0; // first index
-    let i = 1; // second index
+    let j = 0; // first pointer
+    let i = 1; // second pointere
     
     while (i < nums.length) { //while i < length of array 
-        if (nums[j] === 0 && nums[i] !== 0 ) { // if the first index is 0 and the                                                   second index is not equal to zero
+        if (nums[j] === 0 && nums[i] !== 0 ) { // if the first pointer is 0 and the                                                   second pointer is not equal to zero
             nums[j] = nums[i] // replace 0 at that index with next non-zero num
             nums[i] = 0 // let second pointer become the 0 from last index
             j++ // move up the first pointer
