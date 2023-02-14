18b4bd81d5a4af17289f1e32e3d6105a27e1c900
Shea <sheae123@gmail.com>
2023-02-13 23:22:00 -0800

    Time: 56 ms (90.91%), Space: 41.9 MB (67.91%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,9 @@
+function middleNode(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
